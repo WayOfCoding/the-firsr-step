af20958c2f56348f8c9efbf607b90736ab28afe9
shaeling <ying_slaying@sina.cn>
2014-08-27 14:23:07 +0800

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#you can type some brief code in here.this file aim to create a calculator in C
+
+import "stdio.h"
+
+void main(){
+  A = raw_input("please enter a number A:",&d);
+  B = raw_input("please enter a number B:",&d);
+  
+  sum = A + B;
+  
+  printf("the result is:"+ sum,%s);
+
+
+}
