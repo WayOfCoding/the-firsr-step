37395ec914f94b40d399088fdab21a4cfc52a601
shaeling <ying_slaying@sina.cn>
2014-08-29 09:12:47 +0800

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,26 +1,21 @@
 #you can type some brief code in here.this file aim to create a calculator in C
 
-import "stdio.h"
-
-void main(){
-  A = raw_input("please enter a number A:",&d);
-  B = raw_input("please enter a number B:",&d);
-  
-  sum = A + B;
-  
-  printf("the result is:"+ sum,%s);
-
-
-}
-/*#include<stdio.h>
-void main()
+#include <stdio.h>
+main()
 {
-int A,B,S;
-printf("please enter a number A:");
-scanf("%d",&A);
-printf("please enter a number B:");
-scanf("%d",&B);
-S=A+B;
-printf("the result is :");printf("%d\n",S);
+	int A,B,S;
+	printf("please enter a number A:");
+	scanf("%d",&A);
+	printf("please enter a number B:");
+	scanf("%d",&B);
+
+	S = sum(A,B);
+	printf("%d\n",S);
+
 }
-*/
+
+int sum(int n,int m)
+{
+	int S=n+m;
+	return S;
+}
