63feebd71c958b0bf1c9b73a498ed1a1df771bb4
shaeling <ying_slaying@sina.cn>
2014-09-03 13:08:03 +0800

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,34 +4,50 @@
 
 
 #include <stdio.h>
-int sum(int n,int m);
-int difference(int n ,int m);
+int sum(int n, int m);
+int difference(int n, int m);
+int times(int n, int m);
+int division(int n, int m);
 
 
 main()
 {
-	int A,B,S,differ;
+	int A, B, S, differ,time,divise;
 	printf("please enter a number A:");
-	scanf("%d",&A);
+	scanf("%d", &A);
 	printf("please enter a number B:");
-	scanf("%d",&B);
+	scanf("%d", &B);
 
-	S = sum(A,B);
-	differ= difference(A,B);
-	printf("%d\n",S);
-	printf("%d\n",differ);
+	S = sum(A, B);
+	differ = difference(A, B);
+	time = times(A, B);
+	divise = division(A, B);
+
+	printf("%d,%d,%d,%d\n", S, differ, divise, time);
 
 }
 
-int sum(int n,int m)
+int sum(int n, int m)
 {
-	int S=n+m;
+	int S = n + m;
 	return S;
 }
 
-int difference(int n,int m)
+int difference(int n, int m)
 {
 	int differ = n - m;
 	return differ;
-	
+}
+
+int times(int n, int m)
+{
+	int times = n * m;
+	return times;
+}
+
+int division(int n, int m)
+{
+	int divise = n / m;
+	return divise;
+
 }
