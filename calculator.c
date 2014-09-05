19caadeb438aa69484e69a51f4c5a268e8d8e639
shaeling <ying_slaying@sina.cn>
2014-09-05 12:42:10 +0800

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -4,6 +4,7 @@
 
 
 #include <stdio.h>
+#include<conio.h>
 int sum(int n, int m);
 int difference(int n, int m);
 int times(int n, int m);
@@ -12,18 +13,40 @@ int division(int n, int m);
 
 main()
 {
-	int A, B, S, differ,time,divise;
+	int A, B, S, differ,time,divise,operate;
 	printf("please enter a number A:");
 	scanf("%d", &A);
 	printf("please enter a number B:");
 	scanf("%d", &B);
+	printf("请选择要进行的操作：\n");
+	printf("1.加法\n");
+	printf("2.除法\n");
+	printf("3.乘法\n");
+	printf("4.减法\n");
+	printf("0.退出\n");
+	scanf("%d", &operate);
 
-	S = sum(A, B);
-	differ = difference(A, B);
-	time = times(A, B);
-	divise = division(A, B);
-
-	printf("%d,%d,%d,%d\n", S, differ, divise, time);
+	switch (operate)
+	{
+	case 1:
+		S = sum(A, B);
+		printf("%d\n", S);
+		break;
+	case 2:
+		differ = difference(A, B);
+		printf("%d\n", differ);
+		break;
+	case 3:
+		time = times(A, B);
+		printf("%d\n", time);
+		break;
+	case 4:
+		divise = division(A, B);
+		printf("%d\n", divise);
+		break;
+	case 0:
+		break;
+	}	
 
 }
 
